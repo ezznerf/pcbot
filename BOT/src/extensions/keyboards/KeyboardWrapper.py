72dc3a0c3a662b0560a1a8a1f896e931b972176a
ezznerf <126099540+ezznerf@users.noreply.github.com>
2023-04-23 11:47:48 +0300

    Update KeyboardWrapper.py

--- BOT/src/extensions/keyboards/KeyboardWrapper.py
+++ BOT/src/extensions/keyboards/KeyboardWrapper.py
@@ -5,28 +5,42 @@ class KeyboardWrapper:
     def __init__(self, resize_keyboard=True):
         self.__keyboard = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard)
         self.__help = dict()
-      #  self.__brightness = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard)
+        self.__brightness = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard)
+        self.__hotkeys = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard)
+        self.__mouse = ReplyKeyboardMarkup(resize_keyboard=resize_keyboard)
+
 
     def add_button(self, text, description):
-       #inlineButton = InlineKeyboardButton(text)
         button = KeyboardButton(text)
 
         self.__help[text] = description
         self.__keyboard.add(button)
+        self.__brightness.add(button)
+        self.__hotkeys.add(button)
+        self.__mouse.add(button)
 
         return self
 
     @property
     def keyboard(self):
         return self.__keyboard
+   
+   @property
+    def brightness(self):
+        return self.__brightness
 
- #   @property
- #   def brightness(self):
-  #      return self.__brightness
+    @property
+    def hotkeys(self):
+        return self.__hotkeys
+
+    @property
+    def mouse(self):
+        return self.__mouse
 
     @property
     def help(self):
         content = ''
         for key in self.__help:
             content += key + ' - ' + self.__help.get(key) + '\n'
+
         return content
